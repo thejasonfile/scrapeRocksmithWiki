d996b9ba196820a7f566d9dc04a4ce252dd32795
Jason Arnold <jason.arnold.00@gmail.com>
2017-10-26 16:24:21 -0400

    can generate a final obj but some data is incorrect

--- rocksmith.js
+++ rocksmith.js
@@ -7,59 +7,84 @@ const scrape = async () => {
   await page.waitFor(1000);
 
   const result = await page.evaluate(() => {
-    const getRows = () => {
-      return document.querySelectorAll('tbody tr');
+    const getRowsArray = selector => document.querySelectorAll(selector);
+
+    const getCellsArray = row => row.cells;
+
+    const convertToArray = arr => Array.prototype.slice.call(arr);
+
+    const getCellsTextArray = arr => arr.map(item => item.innerText)
+
+    const getCellStatus = index => cellStatus[index];
+
+    const setCellContent = (index, content) => cellStatus[index].text = content;
+
+    const setCellRowspan = (index, num) => cellStatus[index].rowspan = num;
+
+    const removeUnwantedText = (arr, startIndex = 0, index1 = 5, index2 = 7) => {
+      return arr.slice(startIndex, index1).concat(arr.slice(index1 + 1, index2));
     };
 
-    const getCells = (row) => {
-      return row.cells
-    };
+    const rowspanCells = arr => arr.filter(elem => elem.rowspan);
 
-    const getCellStatus = (num) => {
-      return cellStatus[num];
-    }
+    const allData = {};
+    const rows = getRowsArray('tbody tr:nth-child(-n+5)');
 
-    const setCellData = (index, data) => {
-      cellStatus[index].data = data;
-    }
+    const cellStatus = [
+      {text: '', rowspan: 0},
+      {text: '', rowspan: 0},
+      {text: '', rowspan: 0},
+      {text: '', rowspan: 0},
+      {text: '', rowspan: 0},
+      {text: '', rowspan: 0},
+      {text: '', rowspan: 0},
+      {text: '', rowspan: 0}
+    ];
 
-    const setCellRowspan = (index, num) => {
-      cellStatus[index].rowspan = num;
-    }
+    // var row0Cells = convertToArray(getCellsArray(rows[0]));
+    // var row1Cells = convertToArray(getCellsArray(rows[1]));
+    // var row2Cells = convertToArray(getCellsArray(rows[2]));
+    // var row3Cells = convertToArray(getCellsArray(rows[3]));
+    // var row4Cells = convertToArray(getCellsArray(rows[4]));
+    // var row0CellsText = getCellsTextArray(row0Cells);
+    // var row1CellsText = getCellsTextArray(row1Cells);
+    // var row2CellsText = getCellsTextArray(row2Cells);
+    // var row3CellsText = getCellsTextArray(row3Cells);
+    // var row4CellsText = getCellsTextArray(row4Cells);
 
-    const data = [];
-    const rows = getRows();
+    const populateDataObj = () => {
+      rows.forEach((row, index) => {
+        let rowData = [];
+        let rowCells = convertToArray(getCellsArray(row));
+        let rowCellsText = getCellsTextArray(rowCells);
 
-    const cellStatus = {
-      0: {data: '', rowspan: 0},
-      1: {data: '', rowspan: 0},
-      2: {data: '', rowspan: 0},
-      3: {data: '', rowspan: 0},
-      4: {data: '', rowspan: 0},
-      7: {data: '', rowspan: 0}
-    };
-
-    rows.forEach(row => {
-      let cells = getCells(row);
-      cells = Array.prototype.slice.call( cells )
-
-      const cellResult = cells.forEach((cell, index) => {
-        debugger;
-        if (cell.innerText === 'N/A' || cell.innerText === "") {
-          return null;
-        } else if (getCellStatus(index).rowspan > 0) {
-          setCellRowspan(index, getCellStatus(index).rowspan - 1);
-          return getCellStatus(index).data
-        } else if (cell.getAttribute('rowspan')) {
-          setCellData(index, cell.innerText.match(/[^"]/g).join(''));
-          setCellRowspan(index, cell.getAttribute('rowspan') - 1)
-          return getCellStatus(index).data;
-        } else {
-          return cell.innerText.match(/[^"]/g).join('')
-        }
+        rowCells.forEach((cell, index) => {
+          if (getCellStatus(index).rowspan) { //cellStatus has rowspan > 0
+            rowData.push(getCellStatus(index).text);
+            setCellRowspan(index, getCellStatus(index).rowspan - 1)
+          }
+          else if (cell.getAttribute('rowspan')) { //td has rowspan attribute
+            setCellRowspan(index, cell.getAttribute('rowspan'));
+            setCellContent(index, cell.innerText);
+            rowData.push(rowCellsText[index])
+          }
+          else { //cellStatus has rowspan = 0 and td does not have rowspan attribute
+            rowData.push(rowCellsText[index])
+          }
+        });
+        allData[index] = removeUnwantedText(rowData);
       });
-      data.push(cellResult);
-    });
+    }
+
+    populateDataObj();
+    //iterate over each row's cells
+    //get each cells innerText and create a new array
+    //if cell status has any rowspan > 0
+      //splice insert the content into the new array at the index
+      //rowspan - 1
+    //if cell has attriubte 'rowspan' set cellStatus conent and rowspan number
+    //cell.innerText.match(/[^"]/g).join(''));
+    return allData;
   });
 
   browser.close();

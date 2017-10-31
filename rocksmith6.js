180312403c4c421a23c483120d94a145bc95838e
Jason Arnold <jason.arnold.00@gmail.com>
2017-10-31 00:30:20 -0400

    getting 8 cells for most rows, some only get 6

--- rocksmith6.js
+++ rocksmith6.js
@@ -9,7 +9,9 @@ const buildRowsArray = selector => {
 
 const getResult = () => {
   const result = {};
-  const rowsArray = buildRowsArray('tbody tr:nth-child(-n+20):not(:first-child');
+  //const rowsArray = buildRowsArray('tbody tr:nth-child(-n+20):not(:first-child');
+  const rowsArray = buildRowsArray('tbody tr:nth-child(n+950)');
+  //const rowsArray = buildRowsArray('tbody tr:not(:first-child');
 
   rowsArray.forEach((row, index) => {
     const resultArray = [];
@@ -27,13 +29,35 @@ const getResult = () => {
       }
     });
 
-    //check cells for any rowspan values and add them to spanTracker
+    //check cells for any rowspan values
     const cellsWithRowspan = cellsArray.filter(cell => cell.rowSpan > 1);
 
     //if there are any cells with rowspan add them to spanTracker
     cellsWithRowspan.forEach(cell => {
       const cellIndex = cellsArray.indexOf(cell);
       spanTracker[cellIndex].rowspan = cell.rowSpan - 1;
+      spanTracker[cellIndex].text = cell.innerText;
+    });
+
+    //check for exising colspan values to add to cellsArray
+    spanTracker.forEach(obj => {
+      if (obj.colspan > 0) {
+        const newObj = obj;
+        const objIndex = spanTracker.indexOf(newObj);
+        const objText = spanTracker[objIndex].text;
+        cellsArray.splice(objIndex + 1, 0, objText);
+        newObj.colspan -= 1;
+      }
+    });
+
+    //check cells for any colspan values
+    const cellsWithColspan = cellsArray.filter(cell => cell.colSpan > 1);
+
+    //if there are any cells with colspan expand cellsArr and add them to spanTracker
+    cellsWithColspan.forEach(cell => {
+      const cellIndex = cellsArray.indexOf(cell);
+      cellsArray.splice(cellIndex + 1, 0, cell.innerText);
+      spanTracker[cellIndex].colspan = cell.colSpan;
       spanTracker[cellIndex].text = cell.innerText;
     });
 
@@ -45,13 +69,9 @@ const getResult = () => {
         resultArray.push(cell.innerText);
       }
     });
-
+    console.log(resultArray);
     result[index] = resultArray;
-    console.log(result);
-    debugger;
-
   });
-  //debugger;
   return result;
 };
 

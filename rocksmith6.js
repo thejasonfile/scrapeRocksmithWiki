4c37abc35539754d3806855e3038c5b997cd46ff
Jason Arnold <jason.arnold.00@gmail.com>
2017-10-31 14:27:18 -0400

    move colspan before rowspan

--- rocksmith6.js
+++ rocksmith6.js
@@ -10,34 +10,13 @@ const buildRowsArray = selector => {
 const getResult = () => {
   const result = {};
   //const rowsArray = buildRowsArray('tbody tr:nth-child(-n+20):not(:first-child');
-  const rowsArray = buildRowsArray('tbody tr:nth-child(n+950)');
+  const rowsArray = buildRowsArray('tbody tr:nth-child(n+990)');
   //const rowsArray = buildRowsArray('tbody tr:not(:first-child');
 
   rowsArray.forEach((row, index) => {
     const resultArray = [];
     const { cells } = row;
     const cellsArray = convertToArray(cells);
-
-    //check for existing rowspan values to expand cellsArray
-    spanTracker.forEach(obj => {
-      if (obj.rowspan > 0) {
-        const newObj = obj;
-        const objIndex = spanTracker.indexOf(newObj);
-        const objText = spanTracker[objIndex].text;
-        cellsArray.splice(objIndex, 0, objText);
-        newObj.rowspan -= 1;
-      }
-    });
-
-    //check cells for any rowspan values
-    const cellsWithRowspan = cellsArray.filter(cell => cell.rowSpan > 1);
-
-    //if there are any cells with rowspan add them to spanTracker
-    cellsWithRowspan.forEach(cell => {
-      const cellIndex = cellsArray.indexOf(cell);
-      spanTracker[cellIndex].rowspan = cell.rowSpan - 1;
-      spanTracker[cellIndex].text = cell.innerText;
-    });
 
     //check for exising colspan values to add to cellsArray
     spanTracker.forEach(obj => {
@@ -58,6 +37,27 @@ const getResult = () => {
       const cellIndex = cellsArray.indexOf(cell);
       cellsArray.splice(cellIndex + 1, 0, cell.innerText);
       spanTracker[cellIndex].colspan = cell.colSpan;
+      spanTracker[cellIndex].text = cell.innerText;
+    });
+
+    //check for existing rowspan values to expand cellsArray
+    spanTracker.forEach(obj => {
+      if (obj.rowspan > 0) {
+        const newObj = obj;
+        const objIndex = spanTracker.indexOf(newObj);
+        const objText = spanTracker[objIndex].text;
+        cellsArray.splice(objIndex, 0, objText);
+        newObj.rowspan -= 1;
+      }
+    });
+
+    //check cells for any rowspan values
+    const cellsWithRowspan = cellsArray.filter(cell => cell.rowSpan > 1);
+
+    //if there are any cells with rowspan add them to spanTracker
+    cellsWithRowspan.forEach(cell => {
+      const cellIndex = cellsArray.indexOf(cell);
+      spanTracker[cellIndex].rowspan = cell.rowSpan - 1;
       spanTracker[cellIndex].text = cell.innerText;
     });
 

7aebb6e52f7bbcfc5555d58bc01b1b703f1943ae
Jason Arnold <jason.arnold.00@gmail.com>
2017-10-31 16:15:46 -0400

    refactored and result removes paretheses from song string

--- rocksmith6.js
+++ rocksmith6.js
@@ -10,68 +10,75 @@ const buildRowsArray = selector => {
 const getResult = () => {
   const result = {};
   //const rowsArray = buildRowsArray('tbody tr:nth-child(-n+20):not(:first-child');
-  //const rowsArray = buildRowsArray('tbody tr:nth-child(n+990)');
-  const rowsArray = buildRowsArray('tbody tr:not(:first-child');
+  const rowsArray = buildRowsArray('tbody tr:nth-child(n+990)');
+  //const rowsArray = buildRowsArray('tbody tr:not(:first-child');
 
   rowsArray.forEach((row, index) => {
     const resultArray = [];
     const { cells } = row;
     const cellsArray = convertToArray(cells);
 
-    //check for existing rowspan values to expand cellsArray
-    spanTracker.forEach(obj => {
-      if (obj.rowspan > 0) {
-        const newObj = obj;
-        const objIndex = spanTracker.indexOf(newObj);
-        const objText = spanTracker[objIndex].text;
-        cellsArray.splice(objIndex, 0, objText);
-        newObj.rowspan -= 1;
-      }
-    });
+    const expandCellsArray = (span, addNum) => {
+      spanTracker.forEach(obj => {
+        if (obj[span] > 0) {
+          const newObj = obj;
+          const objIndex = spanTracker.indexOf(newObj);
+          const objText = spanTracker[objIndex].text;
+          cellsArray.splice(objIndex + 0, 0, objText);
+          newObj[span] -= 1;
+        }
+      });
+    };
 
-    //check cells for any rowspan values
-    const cellsWithRowspan = cellsArray.filter(cell => cell.rowSpan > 1);
+    const pushToResult = arr => {
+      arr.forEach(cell => {
+        if (typeof cell === 'string') {
+          resultArray.push(cell);
+        } else {
+          resultArray.push(cell.innerText);
+        }
+      });
+    };
 
-    //if there are any cells with rowspan add them to spanTracker
+    const fixSongString = arr => {
+      const fixedArr = arr;
+      fixedArr[0] = fixedArr[0].match(/[^"]/g).join('');
+      return fixedArr;
+    };
+
+    const getCellsWithSpan = span => cellsArray.filter(cell => cell[span] > 1);
+
+    //rowspan
+    const cellsWithRowspan = getCellsWithSpan('rowSpan');
+
+    expandCellsArray('rowspan', 0);
+
     cellsWithRowspan.forEach(cell => {
       const cellIndex = cellsArray.indexOf(cell);
       spanTracker[cellIndex].rowspan = cell.rowSpan - 1;
       spanTracker[cellIndex].text = cell.innerText;
     });
 
-    //check for exising colspan values to add to cellsArray
-    spanTracker.forEach(obj => {
-      if (obj.colspan > 0) {
-        const newObj = obj;
-        const objIndex = spanTracker.indexOf(newObj);
-        const objText = spanTracker[objIndex].text;
-        cellsArray.splice(objIndex + 1, 0, objText);
-        newObj.colspan -= 1;
-      }
-    });
+    //colspan
+    const cellsWithColspan = getCellsWithSpan('colSpan');
 
-    //check cells for any colspan values
-    const cellsWithColspan = cellsArray.filter(cell => cell.colSpan > 1);
+    expandCellsArray('colspan', 1);
 
-    //if there are any cells with colspan expand cellsArr and add them to spanTracker
     cellsWithColspan.forEach(cell => {
       const cellIndex = cellsArray.indexOf(cell);
-      cellsArray.splice(cellIndex + 1, 0, cell.innerText);
       spanTracker[cellIndex].colspan = cell.rowSpan - 1;
       spanTracker[cellIndex].text = cell.innerText;
+      cellsArray.splice(cellIndex + 1, 0, cell.innerText);
     });
 
-    //push text from cellsArray to result Array
-    cellsArray.forEach(cell => {
-      if (typeof cell === 'string') {
-        resultArray.push(cell);
-      } else {
-        resultArray.push(cell.innerText);
-      }
-    });
+    pushToResult(cellsArray);
+
+    fixSongString(resultArray);
+
     console.log(resultArray);
     result[index] = resultArray;
   });
+  
   return result;
 };
 
